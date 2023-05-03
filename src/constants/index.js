91044c1391c6fbf3570309ecfcd4d4e36a884a44
Roman <112611445+Makar789654@users.noreply.github.com>
2023-05-03 17:18:36 +0200

    Add files via upload

--- src/constants/index.js
+++ src/constants/index.js
@@ -93,30 +93,23 @@ const technologies = [
 
 const projects = [
   {
-    name: "Car Rent",
+    name: "Yotube clone",
     description:
-      "Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
+      "Web-based needs.",
     tags: [
       {
         name: "react",
         color: "blue-text-gradient",
       },
-      {
-        name: "mongodb",
-        color: "green-text-gradient",
-      },
-      {
-        name: "tailwind",
-        color: "pink-text-gradient",
-      },
+     
     ],
     image: carrent,
-    source_code_link: "https://github.com/",
+    source_code_link: "https://github.com/Makar789654/youtube-clon",
   },
   {
-    name: "Job IT",
+    name: "E-commerce site",
     description:
-      "Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
+      "Web applicati.",
     tags: [
       {
         name: "react",
@@ -127,33 +120,26 @@ const projects = [
         color: "green-text-gradient",
       },
       {
-        name: "scss",
-        color: "pink-text-gradient",
-      },
-    ],
-    image: jobit,
-    source_code_link: "https://github.com/",
-  },
-  {
-    name: "Trip Guide",
-    description:
-      "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
-    tags: [
-      {
         name: "nextjs",
         color: "blue-text-gradient",
       },
-      {
-        name: "supabase",
-        color: "green-text-gradient",
-      },
+   
+    ],
+    image: jobit,
+    source_code_link: "https://github.com/Makar789654/shop-on-react",
+  },
+  {
+    name: "First site",
+    description:
+      "A ",
+    tags: [
       {
         name: "css",
         color: "pink-text-gradient",
       },
     ],
     image: tripguide,
-    source_code_link: "https://github.com/",
+    source_code_link: "https://github.com/Makar789654/makar7879654.ex1.github.io",
   },
 ];
 
